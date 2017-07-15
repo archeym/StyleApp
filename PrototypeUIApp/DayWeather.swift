8b2d60c67d15b8a2d6c916b9ca56b56e8bb02fee
your name <your@email.com>
2017-07-15 15:15:19 +0100

    Animating TableView

---
+++ PrototypeUIApp/DayWeather.swift
@@ -0,0 +1,17 @@
+ //
+//  DayWeather.swift
+//  PrototypeUIApp
+//
+//  Created by Arkadijs Makarenko on 14/07/2017.
+//  Copyright © 2017 ArchieApps. All rights reserved.
+//
+
+import UIKit
+ 
+ struct DayWeather{
+    var dayName = ""
+    var longDate = ""
+    var weatherIcon: UIImage?
+    var temperature = ""
+    var city = ""
+ }
